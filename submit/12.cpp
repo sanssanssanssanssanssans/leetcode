d80f2338bca8c116f5da5b390fcc1f907cf4cf06
sanssanssanssanssanssans <ajona5427@kakao.com>
2026-05-01 00:42:58 +0900

    asdf

---
+++ submit/12.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sz(x) (int)x.size()
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        map<int, string> mp = {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+
+        string result = "";
+        for (auto it = mp.rbegin(); it != mp.rend(); it++) {
+            int value = it->first;
+            string sym = it->second;
+
+            while (num >= value) {
+                result += sym;
+                num -= value;
+            }
+        }
+
+        return result;
+    }
+};
+
+// for the debug
+
